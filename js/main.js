0893fe82be7e6bdffd7dbd58edb15dbfb92a416d
Gotterdammerung <57160686+Naumov4t@users.noreply.github.com>
2023-09-18 22:07:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let menuBtn = document.querySelector('.header-section__menu-btn');
+let menu = document.querySelector('.mobile-nav');
+
+menuBtn.addEventListener('click', function() {
+    menuBtn.classList.toggle('header-section__menu-btn--active');
+    menu.classList.toggle('mobile-nav--active');
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImxldCBtZW51QnRuID0gZG9jdW1lbnQucXVlcnlTZWxlY3RvcignLmhlYWRlci1zZWN0aW9uX19tZW51LWJ0bicpO1xyXG5sZXQgbWVudSA9IGRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoJy5tb2JpbGUtbmF2Jyk7XHJcblxyXG5tZW51QnRuLmFkZEV2ZW50TGlzdGVuZXIoJ2NsaWNrJywgZnVuY3Rpb24oKSB7XHJcbiAgICBtZW51QnRuLmNsYXNzTGlzdC50b2dnbGUoJ2hlYWRlci1zZWN0aW9uX19tZW51LWJ0bi0tYWN0aXZlJyk7XHJcbiAgICBtZW51LmNsYXNzTGlzdC50b2dnbGUoJ21vYmlsZS1uYXYtLWFjdGl2ZScpO1xyXG59KSJdLCJmaWxlIjoibWFpbi5qcyJ9
